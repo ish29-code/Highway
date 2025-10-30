a9fc258f8be4a91ece6a692bfaf60db00242e02d
ish29-code <ishikadeshpande816@gmail.com>
2025-10-30 21:52:45 +0530

    book

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: (process.env.CORS_ORIGIN || process.env.Deployed_URL || "*")
+    origin: (process.env.CORS_ORIGIN || process.env.DEPLOYED_URL || "*")
       .split(",")
       .map((url) => url.trim()),
   })
